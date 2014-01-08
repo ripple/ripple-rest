0c314d42ebf2f642e5c7c6f6bf129b9676e6c813
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-01-08 13:57:00 +0100

    Started building SimplePayment testing framework

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,41 @@
 module.exports = function(grunt) {
 
+	var jsfiles = ['*.js', 'lib/*.js', 'test/*.js'];
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		mocha: {
-			test: {
-				src: ['test/**/*.js'],
+		jshint: {
+			all: jsfiles,
+			jshintrc: '.jshintrc'
+		},
+		simplemocha: {
+      all: {
+        src: ['test/*.js'],
+        options: {
+          globals: ['should'],
+          timeout: 3000,
+          ignoreLeaks: false,
+          ui: 'bdd',
+          reporter: 'spec'
+        }
+      }
+		},
+		watch: {
+			scripts: {
+				files: jsfiles,
+				tasks: ['jshint', 'simplemocha'],
 				options: {
-					run: true
+					interrupt: true
 				}
 			}
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-mocha');
+	grunt.loadNpmTasks('grunt-simple-mocha');
 
 	grunt.registerTask('default', ['jshint']);
-	grunt.registerTask('test', ['jshint', 'mocha:test']);
+	grunt.registerTask('test', ['jshint', 'simplemocha']);
 
 };
