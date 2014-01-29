4b4f9ce9b384b3fc7a15373544e6bbd9a147efbf
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-01-28 18:36:47 -0800

    [CHORE] Logging errors

--- controllers/errorHandler.js
+++ controllers/errorHandler.js
@@ -2,13 +2,13 @@ module.exports = function(res, error) {
 
   // TODO categorize and format errors
 
-  console.log('Error: ' + JSON.stringify(err_obj));
-
   var err_obj = {
     success: false,
     error: error.message || error.engine_result || error,
     message: error.message || error.engine_result_message || error
   };
+
+  console.log('Error: ', err_obj);
 
   res.send(err_obj);
 
