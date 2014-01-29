f4b7679dc3ee507dab885bcc5e5b998f013391a2
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-01-29 11:38:37 -0800

    [FEATURE] Starting to add additional information to error message

--- controllers/errorHandler.js
+++ controllers/errorHandler.js
@@ -8,6 +8,15 @@ module.exports = function(res, error) {
     message: error.message || error.engine_result_message || error
   };
 
+
+  /* Add info to error messages */
+
+  if (err_obj.error === 'tecPATH_DRY') {
+    err_obj.message = err_obj.message + ' Please ensure that the src_address has sufficient funds (in the src_amount currency, if specified) to execute this transaction.';
+  }
+
+
+
   console.log('Error: ', err_obj);
 
   res.send(err_obj);
