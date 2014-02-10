07d4e7df5e8de97ba63b95e3ea27680ade256b16
wltsmrz <wltsmrz@gmail.com>
2014-02-10 12:29:35 -0800

    Cleanup, add rconsole

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-var nconf = require('nconf'), 
-  sequelizeConnect = require('./db/sequelizeConnect'),
-  fs = require('fs'),
-  express = require('express'),
-  https = require('https'),
-  app = express(),
-  ripple = require('ripple-lib');
 
+require('rconsole');
 
+console.set({
+  facility:        'local7',
+  title:           'ripple-rest-server',
+  stdout:          false,
+  stderr:          true,
+  syslog:          true,
+  syslogHashtags:  false,
+  showTime:        true,
+  showLine:        false,
+  showFile:        true,
+  showTags:        true
+});
+
+var fs               = require('fs');
+var https            = require('https');
+var nconf            = require('nconf');
+var ripple           = require('ripple-lib');
+var sequelizeConnect = require('./db/sequelizeConnect');
+var express          = require('express');
+var app              = express();
 
 /* Process Configuration Options */
 nconf
@@ -14,89 +28,101 @@ nconf
   .env()
   .file({ file: './config.json' })
   .defaults({
-    "PORT": 5990,
-    "NODE_ENV": "development",
-    "rippled": {
-      "local_signing": true,
-      "servers": [{
-        "host": "s_west.ripple.com",
-        "port": 443,
-        "secure": true
+    PORT: 5990,
+    NODE_ENV: 'development',
+    rippled: {
+      local_signing: true,
+      servers: [{
+        host: 's_west.ripple.com',
+        port: 443,
+        secure: true
       }]
     }
   });
 
+if (!nconf.get('rippled')) {
+  throw new Error('config.json must include "rippled" to connect to the Ripple Network');
+}
 
+/* Connect to ripple-lib */
+var remoteOpts = nconf.get('rippled');
+
+remoteOpts.store = {
+  loadAccounts: function(callback) {
+  },
+
+  loadAccount: function(account, callback) {
+  },
+
+  saveAccount: function(account, callback) {
+  }
+}
+
+var remote = new ripple.Remote(remoteOpts);
+
+remote.on('error', function(err) {
+  console.error('ripple-lib Remote error: ' + err);
+});
+
+remote.on('disconnect', function() {
+  console.log('Disconnected from ripple-lib');
+});
+
+remote.on('connect', function() {
+  console.log('Connected to ripple-lib');
+  remote.once('ledger_closed', function() {
+    console.log('Connected to remote rippled at: ', remote._getServer()._opts.url);
+  });
+});
+
+remote.connect();
 
 /* Connect to db */
 var db = sequelizeConnect({
   DATABASE_URL: nconf.get('DATABASE_URL')
 });
 
-
-
 /* Initialize models */
 var OutgoingTx = require('./models/outgoingTx')(db);
 
-
-
-/* Connect to ripple-lib */
-if (!nconf.get('rippled')) {
-  throw(new Error('config.json must include "rippled" to connect to the Ripple Network'));
-}
-
-var remote = new ripple.Remote(nconf.get('rippled'));
-remote.connect();
-
-remote.on('error', function(err){
-  console.log('ripple-lib Remote error: ' + err);
-});
-
-remote.once('connect', function(){
-  console.log('Connected to ripple-lib');
-
-  remote.once('ledger_closed', function(){
-    console.log('Connected to remote rippled at: ', remote._getServer()._opts.url);
-  });
-});
-
-
-
 /* Initialize controllers */
 var TxCtrl = require('./controllers/txCtrl')({
-  remote: remote, 
+  remote: remote,
   OutgoingTx: OutgoingTx
 }),
+
 NotificationCtrl = require('./controllers/notificationCtrl')({
-  remote: remote, 
+  remote: remote,
   OutgoingTx: OutgoingTx,
   port: nconf.get('PORT'),
   environment: nconf.get('NODE_ENV')
 }),
+
 PaymentCtrl = require('./controllers/paymentCtrl')({
   remote: remote,
   OutgoingTx: OutgoingTx
 }),
+
 PathfindCtrl = require('./controllers/pathfindCtrl')({
   remote: remote
 }),
+
 StatusCtrl = require('./controllers/statusCtrl')({
   remote: remote
 });
 
-
-
 /* Express middleware */
-app.use(express.json());
-app.use(express.urlencoded());
-
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+app.configure(function() {
+  app.disable('x-powered-by');
+  app.use(express.json());
+  app.use(express.urlencoded());
 });
 
-
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 
 /* Routes */
 
@@ -104,51 +130,45 @@ app.all('*', function(req, res, next) {
 app.get('/', StatusCtrl.getStatus);
 app.get('/api/v1/status', StatusCtrl.getStatus);
 
-
 /* Ripple Txs */
 app.get('/api/v1/addresses/:address/txs/:tx_hash', TxCtrl.getTx);
 app.post('/api/v1/addresses/:address/txs/', TxCtrl.submitTx);
-
 
 /* Notifications */
 app.get('/api/v1/addresses/:address/next_notification', NotificationCtrl.getNextNotification);
 app.get('/api/v1/addresses/:address/next_notification/:prev_tx_hash', NotificationCtrl.getNextNotification);
 
-
 /* Pathfinding */
 app.get('/api/v1/addresses/:address/payments/options', PathfindCtrl.getPathFind);
-
 
 /* Payments */
 app.get('/api/v1/addresses/:address/payments/:tx_hash', PaymentCtrl.getPayment);
 app.post('/api/v1/addresses/:address/payments', PaymentCtrl.submitPayment);
 
-
-
 /* Configure SSL, if desired */
 if (typeof nconf.get('ssl') === 'object') {
-
-  var key_path = nconf.get('ssl').key_path || './certs/server.key',
-    cert_path = nconf.get('ssl').cert_path || './certs/server.crt';
+  var key_path  = nconf.get('ssl').key_path || './certs/server.key';
+  var cert_path = nconf.get('ssl').cert_path || './certs/server.crt';
 
   if (!fs.existsSync(key_path)) {
-    throw(new Error('Must provide key file and a key_path in the config.json in order to use SSL'));
+    throw new Error('Must provide key file and a key_path in the config.json in order to use SSL');
   }
 
   if (!fs.existsSync(cert_path)) {
-    throw(new Error('Must provide certificate file and a cert_path in the config.json in order to use SSL'));
+    throw new Error('Must provide certificate file and a cert_path in the config.json in order to use SSL');
   }
 
   var sslOptions = {
-    key: fs.readFileSync(key_path),
-    cert: fs.readFileSync(cert_path)
+    key:   fs.readFileSync(key_path),
+    cert:  fs.readFileSync(cert_path)
   };
 
-  https.createServer(sslOptions, app).listen(nconf.get('PORT'));
-  console.log('ripple-rest available over HTTPS at port:' + nconf.get('PORT'));
-
+  https.createServer(sslOptions, app).listen(nconf.get('PORT'), function() {
+    console.log('ripple-rest available over HTTPS at port:' + nconf.get('PORT'));
+  });
 } else {
-  app.listen(nconf.get('PORT'));
-  console.log('ripple-rest available over unsecured HTTP at port:' + nconf.get('PORT'));
+  app.listen(nconf.get('PORT'), function() {
+    console.log('ripple-rest available over unsecured HTTP at port:' + nconf.get('PORT'));
+  });
 }
 
