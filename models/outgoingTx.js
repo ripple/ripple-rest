65c73c494325fa58c56175361af5cf3e207a0949
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-02-25 11:36:04 -0800

    [CHORE] Added migration with renamed fields

--- models/outgoingTx.js
+++ models/outgoingTx.js
@@ -8,12 +8,12 @@ module.exports = function(db) {
     initial_hash: {type: sequelize.TEXT, primaryKey: true, notNull: true},
     submitted_at_ledger: sequelize.INTEGER,
     src_address: sequelize.TEXT,
-    tx_type: sequelize.TEXT,
-    tx_state: sequelize.TEXT, // Updated after submission
+    type: sequelize.TEXT,
+    state: sequelize.TEXT, // Updated after submission
 
     /* Added after submission */
-    tx_result: sequelize.TEXT,
-    tx_hash: sequelize.TEXT
+    result: sequelize.TEXT,
+    hash: sequelize.TEXT
 
   });
 
