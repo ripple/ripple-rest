72be1e908acd10a6a465ddec57052cf4d2119f8a
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-03-05 00:16:03 -0800

    [CHORE] Removing controller tests for now

--- test/controllers/paymentCtrl-test.js
+++
@@ -1,8 +0,0 @@
-/*jshint expr: true*/
-var expect = require('chai').expect,
-  ripple = require('ripple-lib'),
-  PaymentCtrl = require('../../controllers/paymentCtrl');
-
-describe('PaymentCtrl', function(){
-
-});
