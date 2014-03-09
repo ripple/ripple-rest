982ea70cf04d9dd9a61687f2fe4d7fef123d305d
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-03-08 17:12:24 -0800

    [CHORE] Added migrations to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,9 @@
+var fs        = require('fs');
+var pg        = require('pg');
+var sqlite3   = require('sqlite3').verbose();
+var config    = require('./config/config-loader');
+var dbconnect = require('./db/db-connect')({ config: config });
+
 
 module.exports = function(grunt) {
 
@@ -19,14 +25,69 @@ module.exports = function(grunt) {
       local: {
         src: ['test/**/*.test.js']
       }
+    },
+
+    migrate: {
+      options: {
+        dir: 'db/migrations',
+        env: {
+          DATABASE_URL: config.get('DATABASE_URL')
+        }
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-db-migrate');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['setupdb']);
   grunt.registerTask('dev', ['jshint', 'nodemon']);
   grunt.registerTask('test', ['jshint', 'simplemocha:local']);
 
+  grunt.registerTask('setupdb', 'Check if the database is running / exists', function(){
+
+    var done = this.async();
+    var db_url = config.get('DATABASE_URL');
+    dbconnect.connect(function(err, db){
+      if (err) {
+        if (!db_url) {
+          grunt.fail.fatal('No DATABASE_URL specified');
+        } else if (db_url.indexOf('sqlite') !== -1) {
+          grunt.fail.fatal('Sqlite not yet supported');
+          // grunt.task.run('createsqlite');
+        } else {
+          grunt.task.run('migrate:up');
+        }
+      }
+
+      done();
+    });
+  });
+
+  // TODO add support for sqlite
+
+  // grunt.registerTask('createsqlite', 'Create sqlite3 database', function(){
+
+  //   var done = this.async();
+
+  //   var db_url = config.get('DATABASE_URL'),
+  //     match = db_url.match(/sqlite3:\/\/(.*\.sqlite)/),
+  //     db_path;
+  //   if (match) {
+  //     db_path = match[1];
+  //   } else {
+  //     grunt.fail.fatal('Can only create sqlite3 database using this script');
+  //   }
+
+  //   var db = new sqlite3.Database(db_path, function(err){
+  //     if (!err) {
+  //       grunt.log.writeln('Created sqlite3 database at: ' + db_path);        
+  //     }
+
+
+
+  //     done(err);
+  //   });
+  // });
 };
--- db/db-connect.js
+++ db/db-connect.js
@@ -8,30 +8,74 @@ module.exports = function(opts) {
     db_url = config.get('DATABASE_URL'),
     db;
 
-  // Use postgres database if provided, otherwise use sqlite3
-  if (db_url.indexOf('postgres') !== -1) {
+  return {
 
-    // var match = db_url.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/),
-    db = new sequelize(db_url, {
-      logging:  false,
-      native: true,
-      define: {
-        underscored: true
+    connect: function(callback) {
+
+      // Use postgres database if provided, otherwise use sqlite3
+      if (db_url.indexOf('postgres') !== -1) {
+
+        // var match = db_url.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/),
+        db = new sequelize(db_url, {
+          logging:  false,
+          native: true,
+          define: {
+            underscored: true
+          }
+        });
+
+      } else if (db_url.indexOf('sqlite') !== -1) {
+
+        if (fs.existsSync(db_url)) {
+          db = new sequelize(db_url, {
+            logging:  false,
+            define: {
+              underscored: true
+            }
+          });
+        } else {
+          var error = new Error('SQLite3 database does not yet exist. Run grunt to create it');
+          if (callback) {
+            callback(error);
+            return;
+          } else {
+            throw(error);
+          }
+        }
+
+      } else {
+        var error = new Error('Must specify PostgreSQL or SQLite3 DATABASE_URL');
+          if (callback) {
+            callback(error);
+            return;
+          } else {
+            throw(error);
+          }
       }
-    });
 
-  } else {
-    throw(new Error('Must specify postgres DATABASE_URL'));
-  }
+      db.authenticate()
+      .error(function(err){
+        var error = new Error('Cannot connect to database: ' + db_url + '. ' + err);
+        if (callback) {
+          callback(error);
+        } else {
+          throw(error);
+        }
+      })
+      .success(function(){
+        if (callback) {
+          callback();
+        } else {
+          console.log('Connected to database: ' + db_url);
+        }
+      });
 
-  db.authenticate()
-  .error(function(err){
-    throw(new Error('Cannot connect to database: ' + db_url + '. ' + err));
-  })
-  .success(function(){
-    console.log('Connected to database: ' + db_url);
-  });
+      if (!callback) {
+        return db;
+      }
 
-  return db;
+    }
+
+  };
 
 };
