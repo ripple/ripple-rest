847e69006722d529bbd44e21e86e2f617b39928e
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-03-09 23:31:23 -0700

    [CHORE] Making grunt postinstall work

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 var fs        = require('fs');
 var pg        = require('pg');
-var sqlite3   = require('sqlite3').verbose();
+// var sqlite3   = require('sqlite3').verbose();
 var config    = require('./config/config-loader');
 var dbconnect = require('./db/db-connect')({ config: config });
 
@@ -41,29 +41,29 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
 
-  grunt.registerTask('default', ['setupdb']);
+  grunt.registerTask('default', ['migrate:up']);
   grunt.registerTask('dev', ['jshint', 'nodemon']);
   grunt.registerTask('test', ['jshint', 'simplemocha:local']);
 
-  grunt.registerTask('setupdb', 'Check if the database is running / exists', function(){
+  // grunt.registerTask('setupdb', 'Check if the database is running / exists', function(){
 
-    var done = this.async();
-    var db_url = config.get('DATABASE_URL');
-    dbconnect.connect(function(err, db){
-      if (err) {
-        if (!db_url) {
-          grunt.fail.fatal('No DATABASE_URL specified');
-        } else if (db_url.indexOf('sqlite') !== -1) {
-          grunt.fail.fatal('Sqlite not yet supported');
-          // grunt.task.run('createsqlite');
-        } else {
-          grunt.task.run('migrate:up');
-        }
-      }
+  //   var done = this.async();
+  //   var db_url = config.get('DATABASE_URL');
+  //   dbconnect.connect(function(err, db){
+  //     if (err) {
+  //       if (!db_url) {
+  //         grunt.fail.fatal('No DATABASE_URL specified');
+  //       } else if (db_url.indexOf('sqlite') !== -1) {
+  //         grunt.fail.fatal('sqlite3 not yet supported');
+  //         // grunt.task.run('createsqlite');
+  //       } else {
+  //         grunt.task.run('migrate:up');
+  //       }
+  //     }
 
-      done();
-    });
-  });
+  //     done();
+  //   });
+  // });
 
   // TODO add support for sqlite
 
