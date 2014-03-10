cd646353ef3fd5ff94b11a6b822301d04fbfc218
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-03-09 23:38:35 -0700

    [CHORE] Fixed base_url

--- server.js
+++ server.js
@@ -89,11 +89,12 @@ var NotificationsController = require('./controllers/notifications-controller')(
 
 
 /* Endpoints */
-var url_base = (typeof config.get('ssl') === 'object' ? 'https' : 'http') + '://' + config.get('HOST') + (config.get('PORT') ? ':' + config.get('PORT') : '');
 app.get('/', function(req, res){
   res.redirect('/v1');
 });
 app.get('/v1', function(req, res){
+  var url_base = req.protocol + '://' + req.subdomains.join('.') + req.host + (config.get('NODE_ENV') === 'development' && config.get('PORT') ? ':' + config.get('PORT') : '');
+
   res.json({
     endpoints: {
       payments: {
