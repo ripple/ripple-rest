65864c6b0da8ffefd8006af369bd29f171e8b3ca
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-03-10 15:13:45 -0700

    [CHORE] Slight modifications of environment setup

--- env/dev-env/run-server.sh
+++ env/dev-env/run-server.sh
@@ -3,5 +3,4 @@
 export DATABASE_URL="postgres://docker:docker@$DB_1_PORT_5432_TCP_ADDR:$DB_1_PORT_5432_TCP_PORT/docker"
 echo "We are running in $(pwd)"
 npm install
-./node_modules/.bin/db-migrate up --config db/database.json --migrations-dir db/migrations
 supervisor -n exit server.js
