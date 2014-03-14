43a34d9126ec2079ff69a1c4b5c5ce1e6166c92c
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-03-13 18:53:46 -0700

    [CHORE] Major refactoring + working on general account_tx filtering

--- server.js
+++ server.js
@@ -49,7 +49,6 @@ app.use(function(req, res, next){
   }
 });
 
-
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
@@ -221,6 +220,7 @@ app.post('/v1/accounts/:account/settings', SettingsController.changeSettings);
 app.get('/v1/tx/:identifier', TransactionsController.getTransaction);
 app.get('/v1/transaction/:identifier', TransactionsController.getTransaction);
 app.get('/v1/transactions/:identifier', TransactionsController.getTransaction);
+
 
 /* Utils */
 app.get('/v1/uuid', UtilsController.getUuid);
