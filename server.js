1beb0c28fe4b7bbac5aa8bc4fdac496d3ba4e367
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-03-14 10:26:35 -0700

    [FIX] Fixed get transaction bug

--- server.js
+++ server.js
@@ -179,7 +179,7 @@ app.get('/v1', function(req, res) {
         account_notifications: url_base + '/v1/accounts/{account}/notifications/{hash,client_resource_id}'
       },
       standard_ripple_transactions: {
-        transaction:           url_base + '/v1/tx/{hash}'
+        transaction:           url_base + '/v1/transaction/{hash}'
       },
       server: {
         status:                url_base + '/v1/server',
@@ -217,7 +217,7 @@ app.post('/v1/accounts/:account/settings', SettingsController.changeSettings);
 
 
 /* Standard Ripple Transactions */
-app.get('/v1/transaction/:hash', TransactionsController.getTransaction);
+app.get('/v1/transaction/:identifier', TransactionsController.getTransaction);
 
 /* Utils */
 app.get('/v1/uuid', UtilsController.getUuid);
