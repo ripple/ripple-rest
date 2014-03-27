ec05c9b12c39b2cdfffa118c45a553302c4d30ba
wltsmrz <wltsmrz@gmail.com>
2014-03-27 16:28:30 -0700

    Allow --config and --rippled arguments

--- config/config-loader.js
+++ config/config-loader.js
@@ -1,6 +1,7 @@
 var fs    = require('fs');
+var path  = require('path');
+var URL   = require('url');
 var nconf = require('nconf');
-
 
 /** Load Configuration according to the following hierarchy 
  *  (where items higher on the list take precedence)
@@ -11,17 +12,26 @@ var nconf = require('nconf');
  *  4. The defaults defined below
  */
 
-
-nconf
-  .argv()
-  .env();
+nconf.argv().env();
 
 // If config.json exists, load from that
-try {
-  var config_url = './config.json';
-  fs.readFileSync(config_url);
-  nconf.file(config_url);
-} catch (err) {}
+var configPath = nconf.get('config') || path.join(__dirname, '/config.json');
+
+if (fs.existsSync(configPath)) {
+  nconf.file(configPath);
+}
+
+if (nconf.get('rippled')) {
+  var rippledURL = URL.parse(nconf.get('rippled'));
+
+  var opts = {
+    host: rippledURL.hostname,
+    port: Number(rippledURL.port),
+    secure: (rippledURL.protocol === 'wss:')
+  }
+
+  nconf.set('rippled_servers', [ opts ]);
+}
 
 nconf.defaults({
   PORT: 5990,
