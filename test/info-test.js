c78a68441ec0f76055c5da08db245acd82d9ca16
wltsmrz <wltsmrz@gmail.com>
2014-05-14 01:20:44 -0700

    Update info-test

--- test/info-test.js
+++ test/info-test.js
@@ -41,35 +41,36 @@ describe('info', function() {
       json: function(statusCode, obj) {
         expect(statusCode).to.equal(200);
         expect(obj).to.be.an('object');
-        expect(obj).to.eql({ success: true, connected: true });
+        expect(obj).to.deep.equal({ success: true, connected: true });
         done();
       }
     };
 
     info.isConnected($, req, res, function(err) {
-      assert(!err, err);
+      expect(err).to.equal(null, err);
     });
   });
 
   it('serverStatus', function(done) {
     var serverInfo = {
-      info:
-        { build_version: '0.24.0-rc1',
-          complete_ledgers: '32570-6595042',
-          hostid: 'ARTS',
-          last_close: { converge_time_s: 2.007, proposers: 4 },
-          load_factor: 1,
-          peers: 53,
-          pubkey_node: 'n94wWvFUmaKGYrKUGgpv1DyYgDeXRGdACkNQaSe7zJiy5Znio7UC',
-          server_state: 'full',
-          validated_ledger:
-            { age: 5,
-              base_fee_xrp: 0.00001,
-              hash: '4482DEE5362332F54A4036ED57EE1767C9F33CF7CE5A6670355C16CECE381D46',
-              reserve_base_xrp: 20,
-              reserve_inc_xrp: 5,
-              seq: 6595042 },
-              validation_quorum: 3
+      info: {
+        build_version: '0.24.0-rc1',
+        complete_ledgers: '32570-6595042',
+        hostid: 'ARTS',
+        last_close: { converge_time_s: 2.007, proposers: 4 },
+        load_factor: 1,
+        peers: 53,
+        pubkey_node: 'n94wWvFUmaKGYrKUGgpv1DyYgDeXRGdACkNQaSe7zJiy5Znio7UC',
+        server_state: 'full',
+        validated_ledger: {
+          age: 5,
+          base_fee_xrp: 0.00001,
+          hash: '4482DEE5362332F54A4036ED57EE1767C9F33CF7CE5A6670355C16CECE381D46',
+          reserve_base_xrp: 20,
+          reserve_inc_xrp: 5,
+          seq: 6595042
+        },
+        validation_quorum: 3
       }
     };
 
@@ -84,13 +85,15 @@ describe('info', function() {
         expect(statusCode).to.equal(200);
         expect(obj).to.be.an('object');
         expect(obj.rippled_server_status).to.be.an('object');
-        expect(obj.rippled_server_status).to.eql(serverInfo.info);
+        expect(obj.rippled_server_status).to.deep.equal(serverInfo.info);
+        expect(obj.rippled_server_url).to.equal('wss://example.com');
+        expect(obj.api_documentation_url).to.equal('https://github.com/ripple/ripple-rest');
         done();
       }
     };
 
     info.serverStatus($, req, res, function(err) {
-      assert(!err, err);
+      expect(err).to.equal(null, err);
     });
   });
 });
