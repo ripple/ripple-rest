250db3c047eb0b868e1a5e6568c0715475d68ef3
Steven Zeiler <zeiler.steven@gmail.com>
2014-07-16 10:46:45 -0700

    [TEST] Add test for 502 Bad Gateway middleware.

---
+++ test/middleware.js
@@ -0,0 +1,39 @@
+var request = require('supertest');
+var assert = require('assert');
+var app = require(__dirname+'/../lib/express_app.js');
+
+describe('Handling no connection to Rippled', function() {
+  it('should respond with a 502 Bad Gatewayd', function(done) {
+    this.timeout(10000);
+    app.remote.once('connected', function() {
+      app.remote.disconnect(function() {
+        request(app) 
+          .get('/v1/server')
+          .expect(502)
+          .end(function(error, response) {
+            assert.strictEqual(response.body.error, 'No connection to rippled');  
+            assert(!response.body.success);
+            done();
+          });
+      });
+    });
+  });
+
+  it('should response with 200 when reconnected', function(done) {
+    this.timeout(10000);
+    setTimeout(function() {
+      app.remote.connect(function() {
+        request(app) 
+          .get('/v1/server')
+          .expect(200)
+          .end(function(error, response) {
+            console.log(response.body);
+            assert(response.body.success);
+            assert(!response.body.error);
+            done();
+          });
+      });
+    }, 5000);
+  });
+});
+
