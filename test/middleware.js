0f89ab2b3e8547ccd02bb8ee74e49bab88e9bc2e
Steven Zeiler <zeiler.steven@gmail.com>
2014-07-16 13:04:59 -0700

    [TASK] Update error class to RippledConnectionError.

--- test/middleware.js
+++ test/middleware.js
@@ -3,37 +3,34 @@ var assert = require('assert');
 var app = require(__dirname+'/../lib/express_app.js');
 
 describe('Handling no connection to Rippled', function() {
-  it('should respond with a 502 Bad Gatewayd', function(done) {
+  it('should respond with 200 when reconnected', function(done) {
     this.timeout(10000);
-    app.remote.once('connected', function() {
-      app.remote.disconnect(function() {
-        request(app) 
-          .get('/v1/server')
-          .expect(502)
-          .end(function(error, response) {
-            assert.strictEqual(response.body.error, 'No connection to rippled');  
-            assert(!response.body.success);
-            done();
-          });
-      });
+    app.remote.once('connected',function() {
+      request(app) 
+        .get('/v1/server')
+        .expect(200)
+        .end(function(error, response) {
+          console.log(response.body);
+          assert(response.body.success);
+          assert(!response.body.error);
+          done();
+        });
     });
   });
 
-  it('should response with 200 when reconnected', function(done) {
+  it('should respond with a 502 Bad Gatewayd', function(done) {
     this.timeout(10000);
-    setTimeout(function() {
-      app.remote.connect(function() {
-        request(app) 
-          .get('/v1/server')
-          .expect(200)
-          .end(function(error, response) {
-            console.log(response.body);
-            assert(response.body.success);
-            assert(!response.body.error);
-            done();
-          });
-      });
-    }, 5000);
+    app.remote.disconnect(function() {
+      request(app) 
+        .get('/v1/server')
+        .expect(502)
+        .end(function(error, response) {
+          assert.strictEqual(response.body.error, 'RippledConnectionError');  
+          assert(!response.body.success);
+          done();
+        });
+    });
   });
+
 });
 
