edc27f562aef88a0ff347f61f5149a94a4a8bd7f
Steven Zeiler <zeiler.steven@gmail.com>
2014-08-06 12:02:32 -0700

    [TEST] Add empty http tests.

---
+++ test/http/info.js
@@ -0,0 +1,8 @@
+describe('Info HTTP resource', function() {
+  it.skip('should response to isConnected', function(next) {
+  });
+
+  it.skip('should report server status', function(next) {
+  });
+});
+
---
+++ test/http/notifications.js
@@ -0,0 +1,40 @@
+describe('Notifications HTTP resource', function() {
+  it.skip('should respond with an error if there is no connection to rippled', function(next) {
+  });
+
+  it.skip('should respond with an error if the account is missing', function(next) {
+  });
+
+  it.skip('should respond with an error if the account is invalid', function(next) {
+  });
+
+  it.skip('should respond with an error if the identifier is missing', function(next) {
+  });
+
+  it.skip('should respond with an error if the identifier is invalid', function(next) {
+  });
+
+  it.skip('should respond with an error if the transaction corresponding to the given hash did not affect the specified account', function(next) {
+  });
+
+  it.skip('should respond with an error if the ledger containing the base transaction is not in the rippleds complete ledger set', function(next) {
+  });
+
+  it.skip('should include the hashes of the previous and next transactions', function(next) {
+  });
+
+  it.skip('should include urls pointing to the previous and next transactions', function(next) {
+  });
+
+  it.skip('should include the client_resource_id in the JSON response-- but not in the notification body', function(next) {
+  });
+
+  it.skip('should correctly identify outgoing transactions', function(next) {
+  });
+
+  it.skip('should correctly identify incoming transactions', function(next) {
+  });
+
+  it.skip('should correctly identify passthrough transactions', function(next) {
+  }); 
+});
