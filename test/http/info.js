aa1cb06602cd931d62573c7a4d099eceb0168f71
Steven Zeiler <zeiler.steven@gmail.com>
2014-08-08 17:24:01 -0700

    [TEST] That server status returns server info.

--- test/http/info.js
+++ test/http/info.js
@@ -1,28 +1,31 @@
 var http = require('supertest');
 var app = require(__dirname+'/../../lib/express_app.js');
 var assert = require('assert');
+var remote = require(__dirname+'/../../lib/remote.js');
 
 describe('Info HTTP resource', function() {
-  before(function(next) {
-    setTimeout(next, 10000);
+  before(function(done) {
+    remote.connect(done);
   });
-  it.skip('should report server status', function(next) {
-    this.timeout(10000);
+  it('should report server status', function(next) {
     http(app)
       .get('/v1/server')
       .expect(200)
       .end(function(error, response) {
         assert.strictEqual(response.body.success, true);
         assert.strictEqual(response.body.api_documentation_url, 'https://github.com/ripple/ripple-rest');
+        assert(response.body.rippled_server_status);
+        assert(response.body.rippled_server_status.peers);
         next();
       });
   });
-  it('should response to isConnected', function(next) {
+  it.skip('should response to isConnected', function(next) {
     this.timeout(10000);
     http(app)
       .get('v1/server/connected')
       .expect(200)
       .end(function(error, response) {
+        console.log(response);
         console.log(error, response.body);
         assert.strictEqual(response.body.success, true);
         assert.strictEqual(response.body.connected, true);
