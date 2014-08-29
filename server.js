31a327eac4b24228a098407a0f66fdc72dee106f
wltsmrz <wltsmrz@gmail.com>
2014-08-28 18:19:23 -0700

    Update config loading logic

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 var fs     = require('fs');
 var https  = require('https');
 var path   = require('path');
-var app    = require(__dirname+'/lib/express_app.js');
 var config = require(__dirname+'/lib/config-loader');
-
-if (~process.argv.indexOf('--checkconfig')) {
-  // Print configuration and exit
-  console.log(JSON.stringify(config.get(), null, 2));
-  process.exit(0);
-}
+var app = require(__dirname+'/lib/express_app.js');
 
 require('rconsole');
 
