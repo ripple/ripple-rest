a4264816f32de03696ec59e34d068ee7d35973be
wltsmrz <wltsmrz@gmail.com>
2014-09-03 17:46:04 -0700

    New configuration, adapt existing code

--- server.js
+++ server.js
@@ -1,47 +1,58 @@
+var fs = require('fs');
+var path = require('path');
+var config = require('./lib/config-loader');
+
 require('rconsole');
 
 console.set({
-  facility:        'local7',
-  title:           'ripple-rest-server',
-  stdout:          false,
-  stderr:          true,
-  syslog:          true,
-  syslogHashtags:  false,
-  showTime:        true,
-  showLine:        false,
-  showFile:        true,
-  showTags:        true
+  facility: 'local7',
+  title: 'ripple-rest-server',
+  stdout: false,
+  stderr: true,
+  syslog: true,
+  syslogHashtags: false,
+  showTime: true,
+  showLine: true,
+  showFile: true,
+  showTags: false
 });
 
-var fs     = require('fs');
-var https  = require('https');
-var path   = require('path');
-var config = require(__dirname+'/lib/config-loader');
-var app = require(__dirname+'/lib/express_app.js');
+var app = require('./lib/express_app.js');
+var port = config.get('port');
+var host = config.get('host');
 
-/* Configure SSL, if desired */
-if (typeof config.get('ssl') === 'object') {
-  var key_path  = config.get('ssl').key_path || path.join(__dirname, '/certs/server.key');
-  var cert_path = config.get('ssl').cert_path || path.join(__dirname, '/certs/server.crt');
+function loadSSLConfig() {
+  var keyPath  = config.get('ssl').key_path
+  || path.join(__dirname, '/certs/server.key');
 
-  if (!fs.existsSync(key_path)) {
-    throw new Error('Must provide key file and a key_path in the config.json in order to use SSL');
+  var certPath = config.get('ssl').cert_path
+  || path.join(__dirname, '/certs/server.crt');
+
+  if (!fs.existsSync(keyPath)) {
+    console.error('Must specify key_path in order to use SSL');
+    process.exit(1);
   }
 
-  if (!fs.existsSync(cert_path)) {
-    throw new Error('Must provide certificate file and a cert_path in the config.json in order to use SSL');
+  if (!fs.existsSync(certPath)) {
+    console.error('Must specify cert_path in order to use SSL');
+    process.exit(1);
   }
 
-  var sslOptions = {
-    key:   fs.readFileSync(key_path),
-    cert:  fs.readFileSync(cert_path)
+  return {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)
   };
+};
 
-  https.createServer(sslOptions, app).listen(config.get('PORT'), config.get('HOST'), function() {
-    console.log('ripple-rest server listening over HTTPS at port:', config.get('PORT'));
+if (config.get('ssl_enabled')) {
+  require('https').createServer(loadSSLConfig(), app).listen(port, host, function() {
+    console.log('ripple-rest server listening over HTTPS at port:', port);
   });
 } else {
-  app.listen(config.get('PORT'), config.get('HOST'), function() {
-    console.log('ripple-rest server listening over UNSECURED HTTP at port:', config.get('PORT'));
+  app.listen(port, host, function() {
+    console.log('ripple-rest server listening over UNSECURED HTTP at port:', port);
   });
 }
+
+// Connect to Ripple
+app.remote.connect();
