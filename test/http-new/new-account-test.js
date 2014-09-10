4a9d5d09c43dca0edf146831ff5a1aeb784a6c87
Rod Calumpong <rod@ripple.com>
2014-09-10 16:28:03 -0700

    [TASK] Use assertions in test and change spacing to follow style guide

--- test/http-new/new-account-test.js
+++ test/http-new/new-account-test.js
@@ -1,22 +1,23 @@
+var assert    = require('assert');
 var supertest = require('supertest');
-var server = require('./../../lib/app.js');
+var server    = require('./../../lib/app.js');
 
 var app;
 
 before(function() {
-	app = supertest(server);
+  app = supertest(server);
 });
 
 describe('Wallet Generation', function() {
-	it('should fail to create a new wallet', function(done) {
-		app
-		.get('/v1/new')
-		.expect('Content-Type', /json/)
-		.expect(200)
-		.expect(function(res, error) {
-			if (res.body.success) return 'Success was true';
-      if (res.body.error_type !== 'connection_error') return 'Error type was not connection error';
-		})
+  it('should fail to create a new wallet', function(done) {
+    app
+    .get('/v1/new')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect(function(res, error) {
+      assert(res.body.success === false);
+      assert(res.body.error_type === 'connection_error');
+    })
     .end(done);
-	});
+  });
 });
