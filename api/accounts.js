a8be934cb452d1d5424e984d32abcbfa299fc256
Geert Weening <geertweening@gmail.com>
2014-09-11 10:02:06 -0700

    [FIX] cleanup new wallet functionality
    - some stuff went missing during express merge resolution
    - fixes

--- api/accounts.js
+++ api/accounts.js
@@ -1,14 +1,14 @@
-var Wallet = require('ripple-lib').Wallet;
+var Wallet  = require('ripple-lib').Wallet;
+var respond = require('./../lib/response-handler.js');
 
 var controller = {
   generate: function(request, response) {
     var wallet = Wallet.generate();
-    response
-      .status(200)
-      .send({
-        succes: true,
-        account: wallet
-      });
+    if (wallet) {
+      respond.success(response, { account: wallet });
+    } else {
+      respond.apiError(response, 'Could not generate wallet');
+    }
   }
 }
 
