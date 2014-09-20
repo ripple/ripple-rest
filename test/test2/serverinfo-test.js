d3be907ee3160365c7de3ce7bdd65df4f2d08cfd
Geert Weening <geertweening@gmail.com>
2014-09-19 19:50:21 -0700

    [TEST] run test2 tests with mocha from npm test

--- test/test2/serverinfo-test.js
+++ test/test2/serverinfo-test.js
@@ -7,45 +7,61 @@ var lib = require('./fixtures')
 var utils = require('./utils')
 var assert = require('assert');
 
-var rippled;
+describe('server status', function() {
 
-var route = new ee;
-var rippled = new ws.Server({port: 5150});
-before(function(done) {
-  route.on('ping', lib.ping)
-  route.on('subscribe', lib.subscribe)
-  route.on('server_info',lib.server_info)
 
-  rippled.on('connection', lib.connection.bind({route:route}));
-  rippled.on('close',function(){
-    console.log("WS closed")
-  })
+  var rippled;
 
-  _app.remote.once('connect', function() {
-    _app.remote.getServer().once('ledger_closed', function() {
-      console.log("got server's ledger_closed")
-      // proceed to the tests, api is ready
-      done()
+  before(function(done) {
+    var route = new ee;
+    rippled = new ws.Server({port: 5150});
+
+    route.on('ping', lib.ping)
+    route.on('subscribe', lib.subscribe)
+    route.on('server_info',lib.server_info)
+
+    rippled.on('connection', lib.connection.bind({route:route}));
+    rippled.on('close',function(){
+      console.log("WS closed")
+    })
+
+    _app.remote.once('connect', function() {
+      _app.remote.getServer().once('ledger_closed', function() {
+        console.log("got server's ledger_closed")
+        // proceed to the tests, api is ready
+        done();
+      });
     });
+
+    _app.remote._servers = [ ];
+    _app.remote.addServer('ws://localhost:5150');
+    console.log("Connecting remote")
+    _app.remote.connect();
+
   });
 
-  _app.remote._servers = [ ];
-  _app.remote.addServer('ws://localhost:5150');
+  after(function(done) {
+    console.log("Cleanup: closing down")
 
-  console.log("Connecting remote")
+    _app.remote.once('disconnect', function() {
+      rippled.close();
 
-  console.log("Connecting remote")
-  _app.remote.connect();
+      app.get('/v1/server')
+        .end(function(err, resp) {
+          console.log("testing /v1/server after disconnected")
+          console.log(resp.body)
+          var keys = Object.keys(lib.nominal_server_status_response_disconnect);
+          var keyresp = utils.hasKeys(resp.body, keys)
+          assert.equal(keyresp.hasAllKeys, true)
+          done()
+        })
 
-});
 
-after(function(done) {
-  console.log("Cleanup: closing down")
-  _app.remote.disconnect()
-  done()
-});
+    });
 
-describe('server status', function() {
+    _app.remote.disconnect();
+
+  });
 
     it('/v1/server/connected',function(done) {
         console.log("Testing server/connected")
@@ -69,23 +85,7 @@ describe('server status', function() {
             done()
         })
     })
-    it('after disconnect', function(done) {
-        _app.remote.disconnect(function() {
-            console.log("intentional disconnect")
-            done()
-        })
-    })
-    it('/v1/server after disconnect response',function(done) {
-        app.get('/v1/server')
-        .end(function(err, resp) {
-            console.log("testing /v1/server after disconnected")
-            console.log(resp.body)
-            var keys = Object.keys(lib.nominal_server_status_response_disconnect);
-            var keyresp = utils.hasKeys(resp.body, keys)
-            assert.equal(keyresp.hasAllKeys, true) 
-            done()
-        })
-    })
+
 
 })
 
