09e86a5ac56726d4be49db62809bd9e56f369455
Geert Weening <geertweening@gmail.com>
2014-09-19 19:18:15 -0700

    [TASK] run tests by `npm test`

--- test.sh
+++
@@ -1,3 +0,0 @@
-NODE_ENV=test ./node_modules/.bin/istanbul test ./node_modules/.bin/mocha -- --reporter spec --timeout 10000 --slow 500 test/*-test.js &&
-(cd ./test2 && ../node_modules/mocha/bin/mocha test-get_payment.js) &&
-(cd ./test2 && ../node_modules/mocha/bin/mocha test-server_info.js)
