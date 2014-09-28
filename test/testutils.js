95c4bb638719aebe05fea3d402a8a7abd2882e6f
wltsmrz <wltsmrz@gmail.com>
2014-09-28 05:10:15 -0700

    Add new dbinterface

--- test/testutils.js
+++ test/testutils.js
@@ -33,7 +33,7 @@ function setup(done) {
 
   app.get('remote').once('connect', function() {
     app.get('remote').getServer().once('ledger_closed', function() {
-      dbinterface.db.sync().complete(done);
+      setImmediate(done);
     });
     app.get('remote').getServer().emit('message', fixtures.ledgerClose());
   });
