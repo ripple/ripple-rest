2f4c44713b001eb706d1991f1f150584d4bc47d3
Geert Weening <geertweening@gmail.com>
2014-09-29 16:45:04 -0700

    [TASK] refactor payments and server info tests

--- test/_serverinfo-test.js
+++ test/_serverinfo-test.js
@@ -1,30 +1,24 @@
+var supertest = require('supertest');
+var _app = require('./../lib/express_app');
+var app = supertest(_app);
+var ws = require('ws');
+var ee = require('events').EventEmitter;
+var lib = require('./_fixtures.js');
+var utils = require('./utils');
+var assert = require('assert');
+
+var rippled;
+var route = new ee;
+var orderlist = new utils.orderlist;
+
 describe('server status', function() {
 
-    this.timeout(10000)
-    var path = require('path');
-    // override config.json with test one
-    process.env['TEST_CONFIG'] = path.join(__dirname, '/config.json');
-
-    var supertest = require('supertest');
-    var _app = require('./../lib/express_app')
-    var app = supertest(_app)
-    var ws = require('ws');
-    var ee = require('events').EventEmitter;
-    var lib = require('./_fixtures.js')
-    var utils = require('./utils')
-    var assert = require('assert');
-
-  var rippled;
-  var route = new ee;
-  var orderlist = new utils.orderlist;
   before(function(done) {
-    if (_app.remote._servers[0]._url != 'ws://localhost:5150')
-        orderlist.isMock = false
     rippled = new ws.Server({port: 5150});
 
-    route.on('ping', lib.ping)
-    route.on('subscribe', lib.subscribe)
-    route.on('server_info',lib.server_info)
+    route.on('ping', lib.ping);
+    route.on('subscribe', lib.subscribe);
+    route.on('server_info',lib.server_info);
 
     rippled.on('connection', lib.connection.bind({route:route}));
     rippled.on('close',function(){
@@ -37,57 +31,52 @@ describe('server status', function() {
       });
     });
 
-//    _app.remote._servers = [ ];
-//    _app.remote.addServer('ws://localhost:5150');
+    _app.remote._servers = [ ];
+    _app.remote.addServer('ws://localhost:5150');
     _app.remote.connect();
 
   });
 
   after(function(done) {
     _app.remote.once('disconnect', function() {
-      lib.clearInterval()
+      lib.clearInterval();
       rippled.close();
       done()
     });
     _app.remote.disconnect();
-/*
-      app.get('/v1/server')
-        .end(function(err, resp) {
-          var keys = Object.keys(lib.nominal_server_status_response_disconnect);
-          var keyresp = utils.hasKeys(resp.body, keys)
-          assert.equal(keyresp.hasAllKeys, true)
-          done()
-        })
-*/
-    });
 
-    it('/v1/server/connected',function(done) {
-        app.get('/v1/server/connected')
-        .end(function(err, resp) {
-            // check that we have a success
-            assert.strictEqual(resp.body.success, true);
-            var keys = Object.keys(lib.nominal_server_state_response);
-            // check that our keys match
-            assert.deepEqual(Object.keys(resp.body), keys)
-            done();
-        });
-    })
-    it('/v1/server while connected',function(done) {
-        var incoming = function(data,ws) {
-            delete data.id
-            assert.deepEqual(data,{"command":"server_info"})
-            orderlist.mark('server_info') 
-        }
-        orderlist.create([{command:'server_info'}])
-        route.once('server_info',incoming)
-        app.get('/v1/server')
-        .end(function(err, resp) {
-            var keys = Object.keys(lib.nominal_server_status_response);
-            var keyresp = utils.hasKeys(resp.body, keys)
-            assert.equal(keyresp.hasAllKeys, true) 
-            assert.equal(orderlist.test(), true)
-            done()
-        })
-    })
-})
+  });
+
+  it('/v1/server/connected',function(done) {
+    app.get('/v1/server/connected')
+    .end(function(err, resp) {
+      // check that we have a success
+      assert.strictEqual(resp.body.success, true);
+      var keys = Object.keys(lib.nominal_server_state_response);
+      // check that our keys match
+      assert.deepEqual(Object.keys(resp.body), keys);
+      done();
+    });
+  });
+
+  it('/v1/server while connected',function(done) {
+    var incoming = function(data,ws) {
+      delete data.id;
+      assert.deepEqual(data,{"command":"server_info"});
+      orderlist.mark('server_info');
+    };
+
+    orderlist.create([{command:'server_info'}]);
+    route.once('server_info',incoming);
+    app.get('/v1/server')
+    .end(function(err, resp) {
+        var keys = Object.keys(lib.nominal_server_status_response);
+        var keyresp = utils.hasKeys(resp.body, keys);
+        assert.equal(keyresp.hasAllKeys, true);
+        assert.equal(orderlist.test(), true);
+        done();
+    });
+  });
+
+});
 
