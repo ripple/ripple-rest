cb4f12c72f2a7c756651ada49c0cb68454e75587
Geert Weening <geertweening@gmail.com>
2014-10-15 21:56:45 -0700

    [TASK] Protecting against POODLE
    as described in https://gist.github.com/3rd-Eden/715522f6950044da45d8

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var fs      = require('fs');
-var https   = require('https');
-var app     = require('./lib/express_app.js');
-var config  = require('./lib/config-loader');
-var remote  = require('./lib/remote.js');
-var logger  = require('./lib/logger.js').logger;
-var utils   = require('./lib/utils.js');
+var fs        = require('fs');
+var https     = require('https');
+var constants = require('constants')
+var app       = require('./lib/express_app.js');
+var config    = require('./lib/config-loader');
+var remote    = require('./lib/remote.js');
+var logger    = require('./lib/logger.js').logger;
+var utils     = require('./lib/utils.js');
 
 var port = config.get('port') || 5990;
 var host = config.get('host');
@@ -27,7 +28,25 @@ function loadSSLConfig() {
 
   return {
     key: fs.readFileSync(keyPath),
-    cert: fs.readFileSync(certPath)
+    cert: fs.readFileSync(certPath),
+
+    //
+    // Protecting against POODLE in node.js
+    // source: https://gist.github.com/3rd-Eden/715522f6950044da45d8
+    //
+
+    // This is the default secureProtocol used by Node.js, but it might be
+    // sane to specify this by default as it's required if you want to
+    // remove supported protocols from the list. This protocol supports:
+    //
+    // - SSLv2, SSLv3, TLSv1, TLSv1.1 and TLSv1.2
+    //
+    secureProtocol: 'SSLv23_method',
+
+    //
+    // Supply `SSL_OP_NO_SSLv3` constant as secureOption to disable SSLv3
+    // from the list of supported protocols that SSLv23_method supports.
+    secureOptions: constants.SSL_OP_NO_SSLv3
   };
 };
 
