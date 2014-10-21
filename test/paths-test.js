c8d92b3ada57aae171ba36279dad6ac0fb488685
Bo Chen <bochen303@gmail.com>
2014-10-21 12:10:49 -0700

    [TEST] add sanity check tests and update test case descriptions

--- test/paths-test.js
+++ test/paths-test.js
@@ -18,8 +18,8 @@ describe('get payment paths', function() {
   beforeEach(testutils.setup.bind(self));
   afterEach(testutils.teardown.bind(self));
 
-  describe('sending account is not valid', function () {
-    it('should not get payment paths', function (done) {
+  describe('sending account is not valid', function() {
+    it('should not get payment paths', function(done) {
       self.app
         .get('/v1/accounts/' + addresses.INVALID + '/payments/paths/' + addresses.VALID + '/100+USD')
         .expect(testutils.checkStatus(400))
@@ -29,8 +29,8 @@ describe('get payment paths', function() {
     });
   });
 
-  describe('destination account is not valid', function () {
-    it ('should not get payment paths', function (done) {
+  describe('destination account is not valid', function() {
+    it('should not get payment paths', function(done) {
       self.app
         .get('/v1/accounts/' + addresses.VALID + '/payments/paths/' + addresses.INVALID + '/100+USD')
         .expect(testutils.checkStatus(400))
@@ -40,8 +40,8 @@ describe('get payment paths', function() {
     });
   });
 
-  describe('destination amount is not valid', function () {
-    it ('should not get payment paths with missing destination currency', function (done) {
+  describe('destination amount is not valid', function() {
+    it('should not get payment paths with missing destination currency', function(done) {
       self.app
         .get('/v1/accounts/' + addresses.VALID + '/payments/paths/' + addresses.VALID + '/100')
         .expect(testutils.checkStatus(400))
@@ -50,7 +50,7 @@ describe('get payment paths', function() {
         .end(done);
     });
 
-    it ('should not get payment paths with incorrectly formatted amount', function (done) {
+    it('should not get payment paths with incorrectly formatted amount', function(done) {
       self.app
         .get('/v1/accounts/' + addresses.VALID + '/payments/paths/' + addresses.VALID + '/100-USD')
         .expect(testutils.checkStatus(400))
@@ -59,7 +59,7 @@ describe('get payment paths', function() {
         .end(done);
     });
 
-    it ('should not get payment paths with an invalid destination issuer', function (done) {
+    it('should not get payment paths with an invalid destination issuer', function(done) {
       self.app
         .get('/v1/accounts/' + addresses.VALID + '/payments/paths/' + addresses.VALID + '/100+USD+' + addresses.INVALID)
         .expect(testutils.checkStatus(400))
@@ -69,10 +69,10 @@ describe('get payment paths', function() {
     });
   });
 
-  describe ('sending account, destination account, destination amount are all valid', function () {
-    describe ('getting paths for XRP destination amount', function () {
-      it ('should find that destination amount issuer is empty', function (done) {
-        self.wss.once('request_ripple_path_find', function (message, conn) {
+  describe('sending account, destination account, destination amount are all valid', function() {
+    describe('getting paths for XRP destination amount', function() {
+      it('should set sending account and destination account', function(done) {
+        self.wss.once('request_ripple_path_find', function(message, conn) {
           assert.strictEqual(message.command, 'ripple_path_find');
           assert.strictEqual(message.source_account, addresses.VALID);
           assert.strictEqual(message.destination_account, addresses.VALID);
@@ -89,10 +89,40 @@ describe('get payment paths', function() {
           .get('/v1/accounts/' + addresses.VALID + '/payments/paths/' + addresses.VALID + '/100+XRP')
           .expect(testutils.checkStatus(200))
           .expect(testutils.checkHeaders)
-          .end(function (err, res) {
+          .end(function(err, res) {
             if (err) return done(err);
 
-            _.each(res.body.payments, function (paymentObj) {
+            _.each(res.body.payments, function(paymentObj) {
+              assert.strictEqual(paymentObj.source_account, addresses.VALID);
+              assert.strictEqual(paymentObj.destination_account, addresses.VALID);
+            });
+
+            done();
+          });
+      });
+
+      it('should set destination amount issuer to empty string', function(done) {
+        self.wss.once('request_ripple_path_find', function(message, conn) {
+          assert.strictEqual(message.command, 'ripple_path_find');
+          assert.strictEqual(message.source_account, addresses.VALID);
+          assert.strictEqual(message.destination_account, addresses.VALID);
+          conn.send(pathFixtures.generateXRPPaymentPaths(message.id, message.source_account, message.destination_account, message.destination_amount));
+        });
+
+        self.wss.once('request_account_info', function(message, conn) {
+          assert.strictEqual(message.command, 'account_info');
+          assert.strictEqual(message.account, addresses.VALID);
+          conn.send(fixtures.accountInfoResponse(message));
+        });
+
+        self.app
+          .get('/v1/accounts/' + addresses.VALID + '/payments/paths/' + addresses.VALID + '/100+XRP')
+          .expect(testutils.checkStatus(200))
+          .expect(testutils.checkHeaders)
+          .end(function(err, res) {
+            if (err) return done(err);
+
+            _.each(res.body.payments, function(paymentObj) {
               assert.strictEqual(paymentObj.destination_amount.issuer, '');
             });
 
@@ -101,9 +131,9 @@ describe('get payment paths', function() {
       });
     });
 
-    describe ('getting paths for IOU destination amount', function () {
-      it ('should find that destination amount issuer equals provided destination issuer', function (done) {
-        self.wss.once('request_ripple_path_find', function (message, conn) {
+    describe('getting paths for IOU destination amount', function() {
+      it('should set sending account and destination account', function(done) {
+        self.wss.once('request_ripple_path_find', function(message, conn) {
           assert.strictEqual(message.command, 'ripple_path_find');
           assert.strictEqual(message.source_account, addresses.VALID);
           assert.strictEqual(message.destination_account, addresses.VALID);
@@ -114,10 +144,34 @@ describe('get payment paths', function() {
           .get('/v1/accounts/' + addresses.VALID + '/payments/paths/' + addresses.VALID + '/100+USD+' + addresses.ISSUER)
           .expect(testutils.checkStatus(200))
           .expect(testutils.checkHeaders)
-          .end(function (err, res) {
+          .end(function(err, res) {
             if (err) return done(err);
 
-            _.each(res.body.payments, function (paymentObj) {
+            _.each(res.body.payments, function(paymentObj) {
+              assert.strictEqual(paymentObj.source_account, addresses.VALID);
+              assert.strictEqual(paymentObj.destination_account, addresses.VALID);
+            });
+
+            done();
+          });
+      });
+
+      it('should set destination amount issuer to provided destination issuer', function(done) {
+        self.wss.once('request_ripple_path_find', function(message, conn) {
+          assert.strictEqual(message.command, 'ripple_path_find');
+          assert.strictEqual(message.source_account, addresses.VALID);
+          assert.strictEqual(message.destination_account, addresses.VALID);
+          conn.send(pathFixtures.generateIOUPaymentPaths(message.id, message.source_account, message.destination_account, message.destination_amount));
+        });
+
+        self.app
+          .get('/v1/accounts/' + addresses.VALID + '/payments/paths/' + addresses.VALID + '/100+USD+' + addresses.ISSUER)
+          .expect(testutils.checkStatus(200))
+          .expect(testutils.checkHeaders)
+          .end(function(err, res) {
+            if (err) return done(err);
+
+            _.each(res.body.payments, function(paymentObj) {
               assert.strictEqual(paymentObj.destination_amount.issuer, addresses.ISSUER);
             });
 
@@ -125,8 +179,8 @@ describe('get payment paths', function() {
           });
       });
 
-      it ('should find that destination amount issuer equals provided destination issuer when issuer is same as destination account', function (done) {
-        self.wss.once('request_ripple_path_find', function (message, conn) {
+      it('should set destination amount issuer to provided destination issuer when issuer is same as destination account', function(done) {
+        self.wss.once('request_ripple_path_find', function(message, conn) {
           assert.strictEqual(message.command, 'ripple_path_find');
           assert.strictEqual(message.source_account, addresses.VALID);
           assert.strictEqual(message.destination_account, addresses.VALID);
@@ -137,10 +191,10 @@ describe('get payment paths', function() {
           .get('/v1/accounts/' + addresses.VALID + '/payments/paths/' + addresses.VALID + '/100+USD+' + addresses.VALID)
           .expect(testutils.checkStatus(200))
           .expect(testutils.checkHeaders)
-          .end(function (err, res) {
+          .end(function(err, res) {
             if (err) return done(err);
 
-            _.each(res.body.payments, function (paymentObj) {
+            _.each(res.body.payments, function(paymentObj) {
               assert.strictEqual(paymentObj.destination_amount.issuer, addresses.VALID);
             });
 
