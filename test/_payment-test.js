612c6f5e127e4ee0e47475bb8978a4e34dcdeb90
wltsmrz <wltsmrz@gmail.com>
2014-10-23 00:30:49 -0700

    Move utils to the only test that uses them

--- test/_payment-test.js
+++ test/_payment-test.js
@@ -5,9 +5,53 @@ var assert        = require('assert');
 var ws            = require('ws');
 var route         = new (require('events').EventEmitter);
 var fixtures      = require('./fixtures')._payments;
-var testutils     = require('./utils');
 var RL            = require('ripple-lib');
-var orderlist     = new testutils.orderlist;
+
+var testutils = { };
+
+// check if obj has keys
+testutils.hasKeys = function(obj,keys) {
+    var list = Object.keys(obj);
+    var hasAllKeys = true;
+    var missing = {};
+    keys.forEach(function(key) {
+        if (list.indexOf(key) == -1) {
+            hasAllKeys = false
+            missing[key] = true;
+        }
+    })
+    return { hasAllKeys : hasAllKeys, missing : missing }
+}
+
+// used to mark the orderings and count of incoming rippled
+testutils.orderlist = function(list) {
+    // list = [{command:<command>}, ... ]
+    var _list = list;
+    var idx = 0;
+    this.isMock = true;
+    this.create = function(list) {
+        _list = list;
+        idx = 0
+    }
+    this.mark = function(command) {
+        if ((_list[idx]) && (_list[idx].command === command)) {
+            idx++
+        } else {
+            throw new Error("out of order rippled command",command)
+        }
+    }
+    this.test = function() {
+        if (this.isMock)
+        return (idx === _list.length)
+        else return true
+    }
+    this.reset = function() {
+        _list = []
+        idx = 0;
+    }
+};
+
+var orderlist = new testutils.orderlist;
 
 /**
  * Payment tests
