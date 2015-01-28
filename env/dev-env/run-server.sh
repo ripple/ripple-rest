3eae89e756ee3de3e80bcfe930badef65f76c436
Alan Cohen <alan@ripple.com>
2015-01-28 14:25:30 -0800

    [TASK] Remove old deploy/dev config

--- env/dev-env/run-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-#TODO: this overrides any user setting, it shouldn't
-export DATABASE_URL="postgres://docker:docker@$DB_1_PORT_5432_TCP_ADDR:$DB_1_PORT_5432_TCP_PORT/docker"
-echo "We are running in $(pwd)"
-npm install pg
-npm install
-supervisor -n exit server.js
