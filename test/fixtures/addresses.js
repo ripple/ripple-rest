ed96a1b45fbe5051d93421d690175ee8ce00506e
Alan Cohen <alan@ripple.com>
2015-03-19 12:13:37 -0700

    [FIX] Correctly determine when to set SendMax (RLJS-272)
    - SendMax should not be set when the source and destination currencies
      are the same and the issuers are "any that the source is able to use
      and the destination is able to receive"
    
      e.g.,
    
       source_amount: {
          value: '0.0003272934271954',
          currency: 'GWD',
          issuer: ''
       },
       destination_amount: {
          value: '0.0003272934271954',
          currency: 'GWD',
          issuer: 'r4p4gZaWSq8Cs1d8mn1jaGqVU1HUns1ek3'
       }
    
    - SendMax documentation:
      https://ripple.com/build/transactions/#special-issuer-values-for-sendmax-and-amount

--- test/fixtures/addresses.js
+++ test/fixtures/addresses.js
@@ -3,5 +3,7 @@ module.exports = {
   ISSUER: 'r3PDtZSa5LiYp1Ysn1vMuMzB59RzV3W9QH',
   SECRET: 'shsWGZcmZz6YsWWmcnpfr6fLTdtFV',
   INVALID: 'rxxLy6UWsjzxsQrTATf1bwDYSaJMoTGvfY2Q',
-  COUNTERPARTY: 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B'
+  COUNTERPARTY: 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B',
+  VALID2: 'rHrGzm86u7fwsickgTghsS9D7sUuC3VKwE',
+  ISSUER2: 'rBXMZX16zyqthrECN4AY4wxbug7335tCKh'
 };
--- test/unit/rest-converter-test.js
+++ test/unit/rest-converter-test.js
@@ -17,13 +17,23 @@ suite('unit - converter - Rest to Tx', function() {
     });
   });
 
-  test('convert() -- payment with XRP', function(done) {
+  test('convert() -- payment with XRP and no source amount', function(done) {
     restToTxConverter.convert(fixtures.paymentRestXRP, function(err, transaction) {
       assert.strictEqual(err, null);
       assert.deepEqual(transaction.summary(), fixtures.paymentTxXRP);
+      assert.strictEqual(transaction.tx_json.SendMax, void(0));
       done();
     });
   });
+
+  test('convert() -- payment XRP to XRP', function(done) {
+    restToTxConverter.convert(fixtures.paymentRestXRPtoXRP, function(err, transaction) {
+      assert.strictEqual(err, null);
+      assert.strictEqual(transaction.tx_json.SendMax, void(0));
+      done();
+    });
+  });
+
 
   test('convert() -- payment with additional flags', function(done) {
     restToTxConverter.convert(fixtures.paymentRestComplex, function(err, transaction) {
@@ -46,14 +56,16 @@ suite('unit - converter - Rest to Tx', function() {
 
   test('convert() -- payment with currency that has different issuers for source and destination amount', function(done) {
     restToTxConverter.convert(fixtures.exportsPaymentRestIssuers({
-      sourceIssuer: addresses.VALID,
-      destinationIssuer: addresses.COUNTERPARTY
+      sourceAccount: addresses.VALID,
+      destinationAccount: addresses.COUNTERPARTY,
+      sourceIssuer: addresses.VALID2,
+      destinationIssuer: addresses.ISSUER2,
     }), function(err, transaction) {
       assert.strictEqual(err, null);
       assert.deepEqual(transaction.tx_json.SendMax, {
         value: '10',
         currency: 'USD',
-        issuer: addresses.VALID
+        issuer: addresses.VALID2
       });
       done();
     });
@@ -61,8 +73,10 @@ suite('unit - converter - Rest to Tx', function() {
 
   test('convert() -- payment with currency that has different issuers for source and destination amount and a source_slippage of 0.1', function(done) {
     restToTxConverter.convert(fixtures.exportsPaymentRestIssuers({
-      sourceIssuer: addresses.VALID,
-      destinationIssuer: addresses.COUNTERPARTY,
+      sourceAccount: addresses.VALID,
+      destinationAccount: addresses.COUNTERPARTY,
+      sourceIssuer: addresses.VALID2,
+      destinationIssuer: addresses.ISSUER2,
       sourceSlippage: '0.1',
       sourceAmount: '10'
     }), function(err, transaction) {
@@ -70,10 +84,46 @@ suite('unit - converter - Rest to Tx', function() {
       assert.deepEqual(transaction.tx_json.SendMax, {
         value: '10.1',
         currency: 'USD',
-        issuer: addresses.VALID
+        issuer: addresses.VALID2
       });
       done();
     });
   });
+
+  test('convert() -- payment with same currency for source and destination, no issuer for source amount', function(done) {
+    restToTxConverter.convert(fixtures.exportsPaymentRestIssuers({
+      sourceIssuer:  '',
+      destinationAccount: addresses.COUNTERPARTY,
+      destinationIssuer: addresses.COUNTERPARTY
+    }), function(err, transaction) {
+      assert.strictEqual(err, null);
+      assert.strictEqual(transaction.tx_json.SendMax, void(0));
+      done();
+    });
+  });
+
+  test('convert() -- payment with same currency for source and destination, no issuer for source and destination amount', function(done) {
+    restToTxConverter.convert(fixtures.exportsPaymentRestIssuers({
+      sourceIssuer:  '',
+      destinationAccount: addresses.COUNTERPARTY,
+      destinationIssuer: ''
+    }), function(err, transaction) {
+      assert.strictEqual(err, null);
+      assert.strictEqual(transaction.tx_json.SendMax, void(0));
+      done();
+    });
+  });
+
+  test('convert() -- payment with same currency for source and destination, no issuer for destination amount', function(done) {
+    restToTxConverter.convert(fixtures.exportsPaymentRestIssuers({
+      sourceIssuer:  addresses.VALID,
+      destinationAccount: addresses.COUNTERPARTY,
+      destinationIssuer: ''
+    }), function(err, transaction) {
+      assert.strictEqual(transaction.tx_json.SendMax, void(0));
+      done(err);
+    });
+  });
+
 
 });
