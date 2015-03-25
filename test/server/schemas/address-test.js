784a6fd32d0e13991f1bbd8601e8bde9a7329b1f
Alan Cohen <alan@ripple.com>
2015-03-24 17:39:54 -0700

    [V2] Add JSON schema for RippleAddress
    - Move old tests to test-old and create directory for new tests
    - Move schema-validator.js to server/ directory
    - Remove travis linting of test-v1

---
+++ test/server/schemas/address-test.js
@@ -0,0 +1,40 @@
+'use strict';
+var assert = require('assert');
+var validator = require('server/schema-validator');
+
+suite('valid addresses', function() {
+
+  var addresses = [
+    'rfaw1MJk5613LmHeAiuhUPZpD93KCMKgrH',
+    'rf6Dn3kTVy9wL9ZWT9TXWH4ZAaXRJhoVXE',
+    'r98y5GiZ5rtSvazRtGiijE1Mra4VtdzjRB',
+    'rPMemoGY4bS7HY8sjjHD8LhvoJcBwRyYpV',
+    'rsQsbWiVgSQhTDcx76sKFF1gw2dcUJTgkf'
+  ];
+
+  addresses.forEach(function(address) {
+    test(address, function() {
+      assert(validator.isValid(address, 'RippleAddress'));
+    });
+  });
+
+  test('ACCOUNT_ZERO', function() {
+    assert(validator.isValid('rrrrrrrrrrrrrrrrrrrrrhoLvTp', 'RippleAddress'));
+  });
+
+  test('ACCOUNT_ONE', function () {
+    assert(validator.isValid('rrrrrrrrrrrrrrrrrrrrBZbvji', 'RippleAddress'));
+  });
+
+  test('root account', function () {
+    assert(validator.isValid(
+      'rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh', 'RippleAddress'));
+  });
+});
+
+suite('invalid addresses', function () {
+  test('bitcoin address', function () {
+    assert(!validator.isValid(
+      '3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy', 'RippleAddress'));
+  });
+});
