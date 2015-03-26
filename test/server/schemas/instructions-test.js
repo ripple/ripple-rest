1c5b52b4d72b7d31df4ac30c36dd82fe9928e020
Chris Clark <clark@ripple.com>
2015-03-26 13:01:07 -0700

    Add TrustlineRequest schema

--- test/server/schemas/instructions-test.js
+++ test/server/schemas/instructions-test.js
@@ -5,30 +5,30 @@ var validator = require('server/schema-validator');
 var validSequence = 12345;
 var validFee = '1.1';
 
-suite('Options Schema', function() {
-  suite('valid options', function() {
+suite('Instructions Schema', function() {
+  suite('valid instructions', function() {
     test('with sequence', function() {
       assert(
         validator.isValid(
-          {sequence: validSequence}, 'Options'));
+          {sequence: validSequence}, 'Instructions'));
     });
 
     test('with max_fee', function() {
       assert(
         validator.isValid(
-          {max_fee: validFee}, 'Options'));
+          {max_fee: validFee}, 'Instructions'));
     });
 
     test('with fixed_fee', function() {
       assert(
         validator.isValid(
-          {fixed_fee: validFee}, 'Options'));
+          {fixed_fee: validFee}, 'Instructions'));
     });
 
     test('with max_ledger_version', function() {
       assert(
         validator.isValid(
-          {max_ledger_version: validSequence}, 'Options'));
+          {max_ledger_version: validSequence}, 'Instructions'));
     });
 
     test('with sequence, max_ledger_version, max_fee', function() {
@@ -36,27 +36,27 @@ suite('Options Schema', function() {
         validator.isValid({
           sequence: validSequence,
           max_ledger_version: validSequence,
-          max_fee: validFee}, 'Options'));
+          max_fee: validFee}, 'Instructions'));
     });
   });
 
-  suite('invalid options', function () {
+  suite('invalid instructions', function() {
     test('with max_fee and fixed_fee', function() {
       assert(
         !validator.isValid(
-          {max_fee: validFee, fixed_fee: validFee}, 'Options'));
+          {max_fee: validFee, fixed_fee: validFee}, 'Instructions'));
     });
 
     test('with invalid ledger version -- negative', function() {
       assert(
         !validator.isValid(
-          {max_ledger_version: -1}, 'Options'));
+          {max_ledger_version: -1}, 'Instructions'));
     });
 
     test('with invalid ledger version -- float', function() {
       assert(
         !validator.isValid(
-          {max_ledger_version: 1.1}, 'Options'));
+          {max_ledger_version: 1.1}, 'Instructions'));
     });
   });
 });
