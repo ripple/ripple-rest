5899969f40319306301cfcf7b300a1d71d601a58
Alan Cohen <alan@ripple.com>
2015-04-16 15:58:01 -0700

    Fix lint errors in notifications-test.js

--- test/notifications-test.js
+++ test/notifications-test.js
@@ -1,18 +1,20 @@
+/* eslint-disable new-cap */
+/* eslint-disable max-len */
+'use strict';
 var assert = require('assert');
-var ripple = require('ripple-lib');
 var testutils = require('./testutils');
 var fixtures = require('./fixtures').notifications;
 var errors = require('./fixtures').errors;
 var addresses = require('./fixtures').addresses;
 
-const VALID_TRANSACTION_HASH = 'F4AB442A6D4CBB935D66E1DA7309A5FC71C7143ED4049053EC14E3875B0CF9BF';
-const INVALID_TRANSACTION_HASH = 'XF4AB442A6D4CBB935D66E1DA7309A5FC71C7143ED4049053EC14E3875B0CF9BF';
+var VALID_TRANSACTION_HASH = 'F4AB442A6D4CBB935D66E1DA7309A5FC71C7143ED4049053EC14E3875B0CF9BF';
+var INVALID_TRANSACTION_HASH = 'XF4AB442A6D4CBB935D66E1DA7309A5FC71C7143ED4049053EC14E3875B0CF9BF';
 
 suite('get notification', function() {
   var self = this;
 
-  //self.wss: rippled mock
-  //self.app: supertest-enabled REST handler
+  // self.wss: rippled mock
+  // self.app: supertest-enabled REST handler
 
   setup(testutils.setup.bind(self));
   teardown(testutils.teardown.bind(self));
@@ -34,19 +36,6 @@ suite('get notification', function() {
       conn.send(fixtures.ledgerResponse(message));
     });
 
-    function handleLedgerQuery(message, conn) {
-      assert.strictEqual(message.command, 'account_tx');
-      assert.strictEqual(message.account, addresses.VALID);
-
-      assert.strictEqual(message.ledger_index_min, fixtures.LEDGER);
-      assert.strictEqual(message.ledger_index_max, fixtures.LEDGER);
-      assert.strictEqual(message.limit, 200);
-
-      self.wss.on('request_account_tx', handleDirectionalTxQuery);
-
-      conn.send(fixtures.accountTxLedgerResponse(message));
-    };
-
     function handleDirectionalTxQuery(message, conn) {
       assert.strictEqual(message.command, 'account_tx');
       assert.strictEqual(message.account, addresses.VALID);
@@ -66,7 +55,21 @@ suite('get notification', function() {
         default:
           assert(false, 'Invalid ledger_index: ' + message.ledger_index_min);
       }
-    };
+    }
+
+
+    function handleLedgerQuery(message, conn) {
+      assert.strictEqual(message.command, 'account_tx');
+      assert.strictEqual(message.account, addresses.VALID);
+
+      assert.strictEqual(message.ledger_index_min, fixtures.LEDGER);
+      assert.strictEqual(message.ledger_index_max, fixtures.LEDGER);
+      assert.strictEqual(message.limit, 200);
+
+      self.wss.on('request_account_tx', handleDirectionalTxQuery);
+
+      conn.send(fixtures.accountTxLedgerResponse(message));
+    }
 
     self.wss.once('request_account_tx', handleLedgerQuery);
 
@@ -95,19 +98,6 @@ suite('get notification', function() {
       conn.send(fixtures.ledgerResponse(message));
     });
 
-    function handleLedgerQuery(message, conn) {
-      assert.strictEqual(message.command, 'account_tx');
-      assert.strictEqual(message.account, addresses.VALID);
-
-      assert.strictEqual(message.ledger_index_min, fixtures.LEDGER);
-      assert.strictEqual(message.ledger_index_max, fixtures.LEDGER);
-      assert.strictEqual(message.limit, 200);
-
-      self.wss.on('request_account_tx', handleDirectionalTxQuery);
-
-      conn.send(fixtures.accountTxLedgerResponse(message));
-    };
-
     function handleDirectionalTxQuery(message, conn) {
       assert.strictEqual(message.command, 'account_tx');
       assert.strictEqual(message.account, addresses.VALID);
@@ -127,7 +117,20 @@ suite('get notification', function() {
         default:
           assert(false, 'Invalid ledger_index: ' + message.ledger_index_min);
       }
-    };
+    }
+
+    function handleLedgerQuery(message, conn) {
+      assert.strictEqual(message.command, 'account_tx');
+      assert.strictEqual(message.account, addresses.VALID);
+
+      assert.strictEqual(message.ledger_index_min, fixtures.LEDGER);
+      assert.strictEqual(message.ledger_index_max, fixtures.LEDGER);
+      assert.strictEqual(message.limit, 200);
+
+      self.wss.on('request_account_tx', handleDirectionalTxQuery);
+
+      conn.send(fixtures.accountTxLedgerResponse(message));
+    }
 
     self.wss.once('request_account_tx', handleLedgerQuery);
 
@@ -151,9 +154,9 @@ suite('get notification', function() {
       conn.send(fixtures.serverInfoMissingLedgerResponse(message));
     });
 
-    function handleLedgerQuery(message, conn) {
+    function handleLedgerQuery() {
       assert(false, 'Should not request account transactions');
-    };
+    }
 
     self.wss.once('request_account_tx', handleLedgerQuery);
 
@@ -171,17 +174,17 @@ suite('get notification', function() {
   });
 
   test('/accounts/:account/notifications/:identifier -- invalid account', function(done) {
-    self.wss.once('request_tx', function(message, conn) {
+    self.wss.once('request_tx', function() {
       assert(false, 'Should not request transaction');
     });
 
-    self.wss.once('request_server_info', function(message, conn) {
+    self.wss.once('request_server_info', function() {
       assert(false, 'Should not request server info');
     });
 
-    function handleLedgerQuery(message, conn) {
+    function handleLedgerQuery() {
       assert(false, 'Should not request account transactions');
-    };
+    }
 
     self.wss.once('request_account_tx', handleLedgerQuery);
 
@@ -194,17 +197,17 @@ suite('get notification', function() {
   });
 
   test('/accounts/:account/notifications/:identifier -- invalid transaction hash', function(done) {
-    self.wss.once('request_tx', function(message, conn) {
+    self.wss.once('request_tx', function() {
       assert(false, 'Should not request transaction');
     });
 
-    self.wss.once('request_server_info', function(message, conn) {
+    self.wss.once('request_server_info', function() {
       assert(false, 'Should not request server info');
     });
 
-    function handleLedgerQuery(message, conn) {
+    function handleLedgerQuery() {
       assert(false, 'Should not request account transactions');
-    };
+    }
 
     self.wss.once('request_account_tx', handleLedgerQuery);
 
@@ -223,13 +226,13 @@ suite('get notification', function() {
       conn.send(fixtures.transactionNotFoundResponse(message));
     });
 
-    self.wss.once('request_server_info', function(message, conn) {
+    self.wss.once('request_server_info', function() {
       assert(false, 'Should not request server info');
     });
 
-    function handleLedgerQuery(message, conn) {
+    function handleLedgerQuery() {
       assert(false, 'Should not request account transactions');
-    };
+    }
 
     self.wss.once('request_account_tx', handleLedgerQuery);
 
